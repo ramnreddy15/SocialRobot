47a574cf874e14f5fa98f41086c907cbfb9695ef
Ram Reddy <ram.n.reddy15@gmail.com>
2022-12-09 13:14:43 -0500

    Intial control system

--- ControlSystems/bootup.py
+++ ControlSystems/bootup.py
@@ -5,6 +5,17 @@ import heapq
 import time
 #import serial
 
+from gpiozero import Servo
+
+#These are the 5th and 6th pins from the top left with usb facing downwards
+#Use servo.value = val to set the servos position and delays to make things smooth.
+#Range is -1 max speed in one direction and 1 is max speed in other direction
+
+
+Right = Servo(27)
+Left = Servo(17)
+
+
 class Receiver:
   def __init__(self, host, port):
     self.commands = []
@@ -42,33 +53,24 @@ class Receiver:
   def checkCommand(self):
     while True:
       if len(self.commands) > 0:
-        #checking if the checksum of the command
-        #equals the sum of all ascii values of every character 
-        #in command statement
-        pattern = "^[0-5]\|.*\|[0-9]{2}:[0-9]{2}\|" #everything but the checksum value
-        checksum = "\w+$" #checksum value
         popped = heapq.heappop(self.commands) #gets smallest value command
-        com = re.findall(pattern, popped) 
-        numval = re.findall(checksum, popped)
-        numval = numval[0]
-        numval = int(numval,16) #converts hex to int
-        print(com[0])
-        print(sum([ord(i) for i in com[0]]))
-        if numval == sum([ord(i) for i in com[0]]):
-          print("working")
-          heapq.heappush(self.transmit, "Correct|"+popped)
-          heapq.heappush(self.executions, popped)
-          print(self.transmit)
-          print(self.executions)
-        else: 
-          heapq.heappush(self.transmit, "Incorrect|"+popped)
+        heapq.heappush(self.transmit, "Correct|"+popped)
+        heapq.heappush(self.executions, popped)
+        print(self.transmit)
+        print(self.executions)
         
   def telemetryTransmit(self):
     print("Nothing")
-    # while True:
-      # if len(self.transmit) > 0:
-      #   print("Transmit queue", self.transmit)
-      #   self.s.send(bytes(heapq.heappop(self.transmit),'utf-8'))    
+    while True:
+      if len(self.transmit) > 0:
+        print("Transmit queue", self.transmit)
+        self.s.send(bytes(heapq.heappop(self.transmit),'utf-8'))    
+  
+  def moveRobot(self, command):
+    arr = [int(i) for i in command.split(" ")]
+    Left.value = arr[0]
+    Right.value = arr[1]
+    time.sleep(0.1)
 
   def execute(self):
     while True:
@@ -76,6 +78,8 @@ class Receiver:
         command = heapq.heappop(self.executions)
         print(command)
         heapq.heappush(self.transmit, "Executed|"+command)
+        if "move" in command:
+            self.moveRobot(command[5:])
         # if command == "Password A_on_LED":
           # if onAndOfffLed():
           #   print("Executed: ", command)
@@ -83,16 +87,11 @@ class Receiver:
           #   print("Did not execute correctly ", command)
         print("Inside execute",self.executions)
         time.sleep(5)
-        if "Balancing" not in self.executions:
-          heapq.heappush(self.executions, "Balancing")
 
   def balance(self):
     print("Nothing")
 
   def gaitGen(self):
-    print("Nothing")
-
-  def computerVision(self):
     print("Nothing")
 
   def sensorData(self):
@@ -111,7 +110,7 @@ class Receiver:
   def runSimul(self):
     threading.Thread(target=self.telemetryReceive).start()
     threading.Thread(target=self.checkCommand).start()
-    # threading.Thread(target=self.telemetryTransmit).start()
+    threading.Thread(target=self.telemetryTransmit).start()
     threading.Thread(target=self.execute).start()
 
     # threading.Thread(target=self.sensorData).start()
@@ -121,5 +120,5 @@ class Receiver:
     # threading.Thread(target=self.comuterVision).start()
 
 def startBoot():
-  simulation = Receiver('192.168.86.136',12345)
+  simulation = Receiver('172.20.10.2',12345)
   simulation.runSimul()
