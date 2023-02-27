7e7707cf714bb8e78189bdd95eed4ce64714d236
Ram Reddy <ram.n.reddy15@gmail.com>
2023-02-27 14:11:24 -0500

    Done monke stuff

--- Motor/drive_motor.py
+++ Motor/drive_motor.py
@@ -8,15 +8,12 @@ currState = 1500
 ser.write("1500".encode())
 #Control Loop to send PWM values continously to the arduino
 while True:
-    input_value = input("Enter direction (w, a, s, d, k): ")
-
+    input_value = input("Enter direction (w, a, d, k): ")
+    
     if(input_value == "k"):
         ser.write("1500 1500".encode())
 
-    if(input_value == "s"):
-        ser.write("1430 1430".encode())
-
-    if(input_value == "s"):
+    if(input_value == "d"):
         ser.write("1540 1500".encode())
 
     if(input_value == "a"):
